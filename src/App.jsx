1da0d6482f160d0d6f4104336cf5f291e72eecb6
NRnishad <nrnishad106@gmail.com>
2024-11-25 16:07:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import Banner from "./Components/Banner/Banner";
+function App() {
+  return (
+    <div className="App">
+      <NavBar ></NavBar>
+      <Banner></Banner>
+    </div>
+  );
+}
+
+export default App;
