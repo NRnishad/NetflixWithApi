df3367332e530dfc0b96eca76c6daa1eff8bf5eb
NRnishad <nrnishad106@gmail.com>
2024-11-26 10:23:45 +0530

    fetch data frome tmdb using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import "./App.css";
-import NavBar from "./Components/NavBar";
+import NavBar from "./Components/NavBar/NavBar";
 import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
 function App() {
   return (
     <div className="App">
       <NavBar ></NavBar>
       <Banner></Banner>
+      <RowPost></RowPost>
     </div>
   );
 } 
